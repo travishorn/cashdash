1df2c14cdb46081004bd6b9c52795a3b1c2b69a7
Travis Horn <travis@travishorn.com>
2024-03-06 18:13:42 -0600

    Move account form into component

---
+++ src/lib/components/AccountForm.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { indentation } from '$lib';
+
+	/** @type {App.Account[]}*/
+	export let accounts;
+</script>
+
+<form method="post">
+	<div>
+		<label for="parentAccountId">Parent account</label>
+		<select id="parentAccountId" name="parentAccountId">
+			<option value="" style="font-style:italic">None (top-level)</option>
+			{#each accounts as account}
+				<option value={account.id}>{@html indentation(account.depth || 0)}{account.id}</option>
+			{/each}
+		</select>
+	</div>
+
+	<div>
+		<label for="id">Name</label>
+		<input id="id" name="id" type="text" maxlength="100" required />
+	</div>
+
+	<div>
+		<label for="description">Description</label>
+		<input id="description" name="description" type="text" maxlength="255" />
+	</div>
+
+	<button>Save</button>
+	<a href="/accounts">Cancel</a>
+</form>
--- src/routes/accounts/new/+page.svelte
+++ src/routes/accounts/new/+page.svelte
@@ -1,31 +1,10 @@
 <script>
+	import AccountForm from '$lib/components/AccountForm.svelte';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
 <h2>Add new account</h2>
 
-<form method="post">
-	<div>
-		<label for="parentAccountId">Parent account</label>
-		<select id="parentAccountId" name="parentAccountId">
-			<option value="">None (top-level)</option>
-			{#each data.accounts as account}
-				<option value={account.id}>{account.id}</option>
-			{/each}
-		</select>
-	</div>
-
-	<div>
-		<label for="id">Name</label>
-		<input id="id" name="id" type="text" maxlength="100" required />
-	</div>
-
-	<div>
-		<label for="description">Description</label>
-		<input id="description" name="description" type="text" maxlength="255" />
-	</div>
-
-	<button>Save</button>
-	<a href="/accounts">Cancel</a>
-</form>
+<AccountForm accounts={data.accounts} />
